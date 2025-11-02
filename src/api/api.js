044c66a64b0460aa45ec00007304123d589f556d
Nishi Anand <nishi829814@gmail.com>
2025-11-03 01:25:47 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://event-management-system-backend-five.vercel.app/api',
   headers: {
     'Content-Type': 'application/json'
   }
